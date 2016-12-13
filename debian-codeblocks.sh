6767616e004ff8f0ca8bb0c8fd7d6daa275f55b0
cheerii <cheerii@users.noreply.github.com>
2016-12-13 10:28:04 +0100

    Create debian-codeblocks.sh

---
+++ debian-codeblocks.sh
@@ -0,0 +1,8 @@
+echo "d
+apt-get -y install git
+apt-get -y update
+apt-get -y -f install
+apt-get -y install apt-transport-https
+aptitude -y update
+aptitude -y install build-essential
+apt-get -y install gdb
